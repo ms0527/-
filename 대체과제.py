fb5b82e0b3ebbb53ac7469df1d7324198be8f10e
ms0527 <115610254+ms0527@users.noreply.github.com>
2022-10-24 21:24:00 +0900

    Add files via upload

---
+++ 대체과제.py
@@ -0,0 +1,6 @@
+strVal = 'data science'        
+nVal = 12345                
+fVal = 1.2                         
+lVal = ['data', 'science']            
+dVal = {'lecture' : 'data science'}   
+bVal = True                          
